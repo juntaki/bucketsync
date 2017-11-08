9b0c74138b7858e597b336d6b57ece02844fd561
juntaki <me@juntaki.com>
2017-11-08 21:42:56 +0900

    Refactoring

---
+++ lib/logger.go
@@ -0,0 +1,42 @@
+package bucketsync
+
+import (
+	"strings"
+
+	"go.uber.org/zap"
+)
+
+type Logger struct {
+	*zap.Logger
+}
+
+func NewLogger(outputPath string, debug bool) (logger *Logger, err error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{outputPath}
+	config.Development = debug
+
+	zapLogger, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	zap.RedirectStdLog(zapLogger)
+
+	logger = &Logger{
+		Logger: zapLogger,
+	}
+
+	//log.SetOutput(logger)
+	return logger, nil
+}
+
+// Wrap standard log library
+func (l *Logger) Write(input []byte) (int, error) {
+	l.Sugar().Debug(strings.Trim(string(input), "\n"))
+	return len(input), nil
+}
+
+// For aws log library
+func (l *Logger) Log(input ...interface{}) {
+	l.Sugar().Debug(input)
+}
